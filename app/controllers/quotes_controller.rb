14ceadde2d9480b1897dc01a98cefcedf8aac473
munir-muhsin <munirmuhsin@gmail.com>
2019-03-02 01:38:45 +0000

    validation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,17 @@
 class QuotesController < ApplicationController
-
   def index
     @quote = Quote.order("RANDOM()").first
   end
+
   def new
     @quote = Quote.new
   end
+
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+     if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
+    end
     redirect_to root_path
   end
 
